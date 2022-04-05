3a79e81a5861e5dc4a9776afde03bb5126db96f9
franco.capirone@gmail.com <franco.capirone@gmail.com>
2022-04-05 06:54:10 +0000

    xxx
    xxxxxx

---
+++ Capi_001.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+#display(dbutils.fs.ls("dbfs:/FileStore/tables/"))
+# DIR  zip.printdir()
+# importing required modules
+from zipfile import ZipFile
+  
+# specifying the zip file name
+#file_name = "/dbfs/FileStore/tables/ZIP/test_01.zip"
+file_name = "/dbfs/FileStore/tables/test.zip"
+  
+# opening the zip file in READ mode
+with ZipFile(file_name, 'r') as zip:
+    # printing all the contents of the zip file
+    zip.printdir()
+  
+    # extracting all the files
+    print('Extracting all the files now...')
+    zip.extractall("/dbfs/tmp/capi/ZIP")
+    print('Done!')
+    display(dbutils.fs.ls("dbfs:/tmp/capi/ZIP"))
